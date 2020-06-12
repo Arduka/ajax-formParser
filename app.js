a9dbb329508611b487f8f5ca5203c3b51b286370
Arduka <lzp2019@126.com>
2020-06-12 18:43:05 +0800

    Function formParse

---
+++ app.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const { promisify } = require('util');
+const querystring = require('querystring');
+const formParse = require('./formParse');
+
+const server = http.createServer();
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+server.on('request', async (req, res) => {
+    if (req.url == '/Ajax') {
+
+        if (req.method == 'GET') {
+            let pathName = path.join(__dirname, 'ajax.html');
+            let data = await readFile(pathName, 'utf8');
+            res.end(data);
+        }
+
+        if (req.method == 'POST') {
+            // req.setEncoding('binary');
+            let body = [];
+            let boundary = req.headers['content-type'].split('boundary=')[1];
+            //console.log(boundary);
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', async () => {
+                let { filed, file } = formParse(body, boundary);
+                console.log(filed);
+                //console.log(file);
+                try {
+                    let files = file.files;
+                    await writeFile(files[1].filename, files[1].binaryStream, 'binary');
+                    console.log('文件写入成功');
+                } catch (error) {
+                    console.log(error);
+                    res.statusCode = 500;
+                    res.end();
+                }
+                res.end('请求成功');
+            })
+        }
+
+    } else {
+        res.end('not found');
+    }
+
+});
+
+server.listen(8080);
+console.log('服务器启动成功');
---
+++ formParse.js
@@ -0,0 +1,56 @@
+module.exports = function formParse(body, boundary) {
+    //将Buffer类型的数据转化成binary编码格式的字符串
+    let formStr = Buffer.concat(body).toString('binary');
+    let formarr = formStr.split(boundary);
+    //去掉首尾两端的无用字符
+    formarr.shift();
+    formarr.pop();
+    //存储普通key-value
+    let filed = {};
+    //存储文件信息
+    let file = {};
+    for (let item of formarr) {
+        //去除首尾两端的非信息字符
+        item = item.slice(0, -2).trim();
+        //value存储input输入的值
+        let value = '';
+        //不同操作系统换行符不同
+        let a;
+        if ((a = item.indexOf('\r\n\r\n')) != -1) {
+            value = item.slice(a + 4);
+        } else if ((a = item.indexOf('\r\r')) != -1) {
+            value = item.slice(a + 2);
+        } else if ((a = item.indexOf('\n\n')) != -1) {
+            value = item.slice(a + 2);
+        }
+        //正则匹配，组中内容
+        let key = item.match(/name="([^"]+)"/)[1];
+        if (item.indexOf('filename') == -1) {
+            if (!(key in filed)) {
+                //将二进制字符串转化成utf8格式的字符串
+                filed[key] = Buffer.from(value,'binary').toString('utf8');
+            } else {
+                //将复选框的数据放入一个数组中
+                let arr = [];
+                filed[key] = arr.concat(filed[key], value);
+            }
+        } else {
+            let filename_b = item.match(/filename="([^"]*)"/)[1];
+            //解决中文文件名乱码的问题
+            let filename = Buffer.from(filename_b,'binary').toString();
+            let contentType = item.slice(item.indexOf('Content-Type:'), a);
+            let obj = {};
+            obj.filename = filename;
+            obj.contentType = contentType;
+            obj.binaryStream = value;//文件的二进制数据
+            if (!(key in file)) {
+                file[key] = obj;
+            } else {
+                //用于多文件上传
+                let arr = [];
+                file[key] = arr.concat(file[key], obj);
+            }
+        }
+    }
+    return { filed, file };
+}
